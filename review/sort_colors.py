52cc38a198aa546cf3f9223de25c9a730a69e8e5
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-08-22 13:06:13 -0400

    3 sum and non-freq sol for sorting colors

--- review/sort_colors.py
+++ review/sort_colors.py
@@ -11,3 +11,26 @@ class Solution:
             nums[idx : idx + freq] = [color] * freq
             idx += freq
 
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        l = 0
+        r = len(nums) - 1
+        mid = 0
+
+        while mid <= r:
+            if nums[mid] == 0:
+                nums[mid], nums[l] = nums[l], nums[mid]
+                l += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[r] = nums[r], nums[mid]
+                r -= 1
+        return nums
+
+
