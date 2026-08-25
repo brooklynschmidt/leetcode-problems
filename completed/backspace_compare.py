23d9c769d07a47626e8594690add3e3b6b28e00f
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-08-24 21:36:00 -0400

    backspace_compare

---
+++ completed/backspace_compare.py
@@ -0,0 +1,23 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s_stack = []
+        t_stack = []
+
+        for char in s:
+            if char != '#':
+                s_stack.append(char)
+            else:
+                if s_stack:
+                    s_stack.pop()
+        for char in t:
+            if char != '#':
+                t_stack.append(char)
+            else:
+                if t_stack:
+                    t_stack.pop()
+        
+        if s_stack == t_stack:
+            return True
+        else:
+            return False
+        
